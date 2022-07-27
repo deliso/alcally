be5bc3cb629ccfb02087c9915cab1e1695b85c66
deliso <s.moraleslopez.bcn@gmail.com+>
2022-07-27 18:32:42 +0200

    feat(schema.prisma): connect to prisma
    connect to pg database and use test schemas to migrate shadow db

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,10 @@
-const Express = require('express');
+import Express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = Express();
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
 });
+
+const prisma = new PrismaClient();
