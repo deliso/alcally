6216b3bbb75cc32f01124ce3746e15ddd6f37412
deliso <s.moraleslopez.bcn@gmail.com>
2022-08-30 19:39:38 +0200

    feat(be user controllers): add controllers for signup and signin

--- server/controllers/user.controllers.ts
+++ server/controllers/user.controllers.ts
@@ -1,25 +1,64 @@
 import { PrismaClient } from '@prisma/client';
-import { User } from '../../types/types';
+import { Company, User } from '../../types/types';
 import { Request, Response } from 'express';
+import * as bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
-const user = async (req: Request, res: Response) => {
+const signup = async (req: Request, res: Response) => {
   const userData: User = req.body;
+  const user = await prisma.user.findUnique({
+    where: {
+      email: userData.email,
+    },
+  });
+  if (user) {
+    res.status(409).send({ error: 409, message: 'User already exists' });
+  }
+  if (userData.password === '') throw new Error();
+  const hash = await bcrypt.hash(userData.password, 10);
+  userData.password = hash;
   try {
-    await prisma.user.create({
+    const newUser = await prisma.user.create({
       data: userData,
     });
     res.status(201);
-    res.end('ok');
+    res.send(newUser);
   } catch (error) {
     console.log(error);
+    res.status(400).send({ error, message: 'Could not create user' });
   }
 };
 
-const getUsers = async (req: Request, res: Response) => {
-  const allUsers: User[] = await prisma.user.findMany();
-  res.send(allUsers);
+// const getUsers = async (req: Request, res: Response) => {
+//   const allUsers: User[] = await prisma.user.findMany();
+//   res.send(allUsers);
+// };
+
+const signin = async (req: Request, res: Response) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        uid: req.body.uid,
+      },
+      include: { companies: true },
+    });
+    if (user && req.body.password) {
+      const validatedPass = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+      if (!validatedPass) throw new Error('Credentials incorrect');
+    }
+    if (!user) throw new Error('Credentials incorrect');
+    res.send(user);
+  } catch (e) {
+    console.error('error', e);
+    if (e) {
+      console.log(e);
+      throw new Error();
+    }
+  }
 };
 
-export default { user, getUsers };
+export default { signup, signin };
---
+++ server/prisma/migrations/20220830110552_users/migration.sql
@@ -0,0 +1,109 @@
+-- CreateEnum
+CREATE TYPE "Type" AS ENUM ('SA', 'SL');
+
+-- CreateEnum
+CREATE TYPE "Category" AS ENUM ('ACCOUNTS', 'BOOKS', 'APPOINTMENT');
+
+-- CreateEnum
+CREATE TYPE "Frequency" AS ENUM ('DAILY', 'WEEKLY', 'MONTHLY', 'QUARTERLY', 'YEARLY');
+
+-- CreateEnum
+CREATE TYPE "Body" AS ENUM ('BOD', 'J_D', 'J_S_D', 'S_D', 'SH', 'S_SH', 'AU');
+
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('CHAIRMAN', 'DIRECTOR', 'SECRETARY');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Company" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" "Type" NOT NULL,
+    "audit" BOOLEAN NOT NULL,
+    "year_end_day" INTEGER NOT NULL,
+    "year_end_month" INTEGER NOT NULL,
+    "nif" TEXT NOT NULL,
+    "cnae" INTEGER NOT NULL,
+    "sole" BOOLEAN NOT NULL,
+    "mgmt" "Body" NOT NULL,
+    "mgmt_rem" BOOLEAN NOT NULL,
+    "mgmt_num" INTEGER NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Company_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Action" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "due_year" INTEGER NOT NULL,
+    "due_month" INTEGER NOT NULL,
+    "due_day" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "companyId" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "hidden" BOOLEAN NOT NULL DEFAULT false,
+    "category" "Category" NOT NULL,
+    "frequency" "Frequency" NOT NULL,
+
+    CONSTRAINT "Action_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Director" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "surname" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+    "body" "Body" NOT NULL,
+    "active" BOOLEAN NOT NULL,
+    "companyId" TEXT NOT NULL,
+    "appointment_year" INTEGER NOT NULL,
+    "appointment_month" INTEGER NOT NULL,
+    "appointment_day" INTEGER NOT NULL,
+    "expiry_year" INTEGER NOT NULL,
+    "expiry_month" INTEGER NOT NULL,
+    "expiry_day" INTEGER NOT NULL,
+    "nif" TEXT NOT NULL,
+    "dir_rem" BOOLEAN NOT NULL,
+    "dir_indef" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Director_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Company_name_key" ON "Company"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Company_nif_key" ON "Company"("nif");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Company_userId_key" ON "Company"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Director_nif_key" ON "Director"("nif");
+
+-- AddForeignKey
+ALTER TABLE "Company" ADD CONSTRAINT "Company_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Action" ADD CONSTRAINT "Action_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Director" ADD CONSTRAINT "Director_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE CASCADE ON UPDATE CASCADE;
